368f32ead06b0e730de4a27dc9f119b77add80e9
Daniel Herschel <danielh2772@gmail.com>
2026-01-03 14:56:57 +0200

    finished hw

--- Network.java
+++ Network.java
@@ -30,7 +30,11 @@ public class Network {
      *  If there is no such user, returns null.
      *  Notice that the method receives a String, and returns a User object. */
     public User getUser(String name) {
-        //// Replace the following statement with your code
+        for (User user : users) {
+            if (user != null && user.getName().equals(name)) {
+                return user;
+            }
+        }
         return null;
     }
 
@@ -39,37 +43,97 @@ public class Network {
     *  If the given name is already a user in this network, does nothing and returns false;
     *  Otherwise, creates a new user with the given name, adds the user to this network, and returns true. */
     public boolean addUser(String name) {
-        //// Replace the following statement with your code
-        return false;
+        if (userCount >= users.length) {
+            return false;
+        }
+        if (getUser(name) != null) {
+            return false;
+        }
+
+        users[userCount] = new User(name);
+        userCount++;
+        return true;
     }
 
     /** Makes the user with name1 follow the user with name2. If successful, returns true.
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
-        //// Replace the following statement with your code
-        return false;
+        User user1 = getUser(name1);
+        User user2 = getUser(name2);
+
+        if (user1 == null || user2 == null) {
+            return false;
+        }
+
+        return user1.addFollowee(name2);
     }
     
     /** For the user with the given name, recommends another user to follow. The recommended user is
      *  the user that has the maximal mutual number of followees as the user with the given name. */
     public String recommendWhoToFollow(String name) {
-        //// Replace the following statement with your code
-        return null;
+        User user = getUser(name);
+
+        if (user == null) {
+            return null;
+        }
+
+        int maxMutuals= 0;
+        User recommendedUser = null;
+        for (User u : users) {
+            if (u == user || u == null) {
+                continue;
+            }
+
+            int mutuals = u.countMutual(user);
+            if (mutuals > maxMutuals) {
+                maxMutuals = mutuals;
+                recommendedUser = u;
+            }
+        }
+
+        return recommendedUser == null ? null : recommendedUser.getName();
     }
 
     /** Computes and returns the name of the most popular user in this network: 
      *  The user who appears the most in the follow lists of all the users. */
     public String mostPopularUser() {
-        //// Replace the following statement with your code
-        return null;
+        String mostPopularName = null;
+        int maxFollowers = 0;
+        for (User u : users) {
+            if (u == null) {
+                continue;
+            }
+
+            int followersCount = followeeCount(u.getName());
+            if (followersCount > maxFollowers) {
+                maxFollowers = followersCount;
+                mostPopularName = u.getName();
+            }
+        }
+
+        return mostPopularName;
     }
 
     /** Returns the number of times that the given name appears in the follows lists of all
      *  the users in this network. Note: A name can appear 0 or 1 times in each list. */
     private int followeeCount(String name) {
-        //// Replace the following statement with your code
-        return 0;
+        User user = getUser(name);
+
+        if (user == null) {
+            return 0;
+        }
+
+        int followersCount = 0;
+        for (User u : users) {
+            if (u == null || u == user) {
+                continue;
+            }
+
+            followersCount += u.follows(name) ? 1 : 0;
+        }
+
+        return followersCount;
     }
 
     // Returns a textual description of all the users in this network, and who they follow.
